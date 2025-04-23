64e1aa50e16c334f05c1bd2b009a6d28ce12a69e
laboratorio <laboratorio@UPOAULA10710>
2025-04-23 08:38:44 -0500

    Agregar archivos de proyecto.

---
+++ HerreraR - ORMclaseBaseDatos/Models/Carrera.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HerreraR___ORMclaseBaseDatos.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Nombre { get; set; }
+        public int FacultadId { get; set; }
+        [ForeignKey("FacultadId")]
+        public Facultad Facultad { get; set; }
+    }
+}
---
+++ HerreraR - ORMclaseBaseDatos/Models/Facultad.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HerreraR___ORMclaseBaseDatos.Models
+{
+    public class Facultad
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+    }
+}
